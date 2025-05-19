c45ae5a42bd5432146521f2de08cef7610518309
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-05-19 10:45:12 -0300

    Update Binary_search.py

--- Binary_search.py
+++ Binary_search.py
@@ -1,6 +1,6 @@
 def binary_search(array,item, begin=0, end= None, loops=0):
     if end is None:
-        end= len(lista) -1
+        end= len(array) -1
 
     if begin <= end:
         m = (begin + end)//2
@@ -12,7 +12,9 @@ def binary_search(array,item, begin=0, end= None, loops=0):
         else:
             return binary_search(array,item, begin,m -1 , loops)
     
-
+#testes
 lista = [1,2,3,4,5,6,7,8,9]
+lista2 = [1,5,9,13,17,21,25,29,33,37,41,45,49,53,57,61,65,69,73,77,81,85,89,93,97]
+lista3 = []
 
-print(binary_search(lista, 5))
+print(binary_search(lista3, 5))
