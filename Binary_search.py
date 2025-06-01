99adb172dcb2c68cd3b6b63411cf8ca133694396
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-06-01 15:30:24 -0300

    complemento

--- Binary_search.py
+++ Binary_search.py
@@ -3,7 +3,9 @@ def binarySearch(list,item,begin,end,comparations):
 
     if begin <=end:
         middle = (begin+end)//2
-
+        if len(list) == 0:
+            return None
+        
         if list[middle] == item:
             return Lista1[middle], comparations
         elif list[middle] > item:
@@ -16,8 +18,8 @@ def binarySearch(list,item,begin,end,comparations):
 #testes
 Lista1 = [11,22,33,44,55,66,77,88,99,101,103,124,156,157,177,180,188,199]
 Lista2 =[]
-Lista3 = [11,22,33,44,55,66,77,88,99,101,103,124,156,157,177,180,188,199]
+
 if __name__ == "__main__":
     print(binarySearch(Lista1,199,0,len(Lista1)-1,0))
-    print(binarySearch(Lista1,7,0,len(Lista1)-1,0))
+    print(binarySearch(Lista2,7,0,len(Lista1)-1,0))
     print(binarySearch(Lista1,33,0,len(Lista1)-1,0))
