69878885f45c9d1bf51017b2507e41b3a2bfdd17
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-06-01 15:25:18 -0300

    adição de testes

--- Binary_search.py
+++ Binary_search.py
@@ -13,7 +13,11 @@ def binarySearch(list,item,begin,end,comparations):
         else:
             return None
 
+#testes
 Lista1 = [11,22,33,44,55,66,77,88,99,101,103,124,156,157,177,180,188,199]
-
+Lista2 =[]
+Lista3 = [11,22,33,44,55,66,77,88,99,101,103,124,156,157,177,180,188,199]
 if __name__ == "__main__":
     print(binarySearch(Lista1,199,0,len(Lista1)-1,0))
+    print(binarySearch(Lista1,7,0,len(Lista1)-1,0))
+    print(binarySearch(Lista1,33,0,len(Lista1)-1,0))
