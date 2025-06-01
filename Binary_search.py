23f79cdb69f9f849c8df6a3ece5775390e977977
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-06-01 15:18:09 -0300

    modificações estruturais

--- Binary_search.py
+++ Binary_search.py
@@ -1,20 +1,19 @@
-def binary_search(array,item, begin=0, end= None, loops=0):
-    if end is None:
-        end= len(array) -1
+def binarySearch(list,item,begin,end,comparations):
+    comparations +=1
 
-    if begin <= end:
-        m = (begin + end)//2
-        loops+=1
-        if array[m] == item:
-            return f"numero encontrado no indice {m} e loops para encontrar = {loops}"
-        if array[m] < item:
-            return binary_search(array,item, m + 1 , end, loops)
+    if begin <=end:
+        middle = (begin+end)//2
+
+        if list[middle] == item:
+            return Lista1[middle], comparations
+        elif list[middle] > item:
+            return binarySearch(list,item,begin,middle - 1,comparations)
+        elif list[middle] < item:
+            return binarySearch(list,item,middle + 1,end,comparations)
         else:
-            return binary_search(array,item, begin,m -1 , loops)
-    
-#testes
-lista = [1,2,3,4,5,6,7,8,9]
-lista2 = [1,5,9,13,17,21,25,29,33,37,41,45,49,53,57,61,65,69,73,77,81,85,89,93,97]
-lista3 = []
+            return None
 
-print(binary_search(lista3, 5))
+Lista1 = [11,22,33,44,55,66,77,88,99,101,103,124,156,157,177,180,188,199]
+
+if __name__ == "__main__":
+    print(binarySearch(Lista1,199,0,len(Lista1)-1,0))
