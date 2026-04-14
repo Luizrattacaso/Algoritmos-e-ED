0181e64383ce21ba8fca45d491074a59b19703ff
Luiz Rattacaso <rattacasoluiz03@gmail,com>
2026-04-13 21:06:10 -0300

    teste para função insert

--- arvores/main.py
+++ arvores/main.py
@@ -1,11 +1,13 @@
 from Classes import Tree, Node
 from operacoes_basicas.search import search, iterative_search
+from operacoes_basicas.insert import insert_node
 
 t = Tree()
 
 t.root = Node(10)
-t.root.left = Node(5)
-t.root.right = Node(15)
+
+insert_node(t, Node(5))
+insert_node(t, Node(15))
 
 # busca recursiva
 resultado = search(t.root, 5)
@@ -14,3 +16,7 @@ print(resultado.key if resultado else "Não encontrado")
 # busca iterativa
 resultado = iterative_search(t.root, 15)
 print(resultado.key if resultado else "Não encontrado")
+
+#inserção de nodes 
+print(t.root.left.key) #5
+print(t.root.right.key) #15
